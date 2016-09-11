82bd21ab5486ac4fff3b7cdf55a6ed0ad49cd56d
Len Vacha <elvy360@yahoo.com>
2016-09-10 19:59:37 -0500

    more agg calc error diag

--- map.js
+++ map.js
@@ -95,15 +95,17 @@ jQuery.noConflict();
     // create obj from raw census data xml
     var parseCensus = function(entry) {
       censusObj = {}
-      censusObj.communityArea = entry.find('community_area_name').text();
-      censusObj.communityAreaNumber = entry.find('ca').text();
-      censusObj.percentHousingCrowded = entry.find('percent_of_housing_crowded').text();
-      censusObj.percentHouseholdsBelowPoverty = entry.find('percent_households_below_poverty').text();
-      censusObj.percentAged16Unemployed = entry.find('percent_aged_16_unemployed').text();
-      censusObj.percentAged25NoDiploma = entry.find('percent_aged_25_without_high_school_diploma').text();
-      censusObj.percentAgedUnder18orOver64 = entry.find('percent_aged_under_18_or_over_64').text();
-      censusObj.perCapitaIncome = entry.find('per_capita_income_').text();
-      censusObj.hardshipIndex = entry.find('hardship_index').text();
+      if(entry.find('ca').text() != null) {
+        censusObj.communityArea = entry.find('community_area_name').text();
+        censusObj.communityAreaNumber = entry.find('ca').text();
+        censusObj.percentHousingCrowded = entry.find('percent_of_housing_crowded').text();
+        censusObj.percentHouseholdsBelowPoverty = entry.find('percent_households_below_poverty').text();
+        censusObj.percentAged16Unemployed = entry.find('percent_aged_16_unemployed').text();
+        censusObj.percentAged25NoDiploma = entry.find('percent_aged_25_without_high_school_diploma').text();
+        censusObj.percentAgedUnder18orOver64 = entry.find('percent_aged_under_18_or_over_64').text();
+        censusObj.perCapitaIncome = entry.find('per_capita_income_').text();
+        censusObj.hardshipIndex = entry.find('hardship_index').text();
+      }
       
       return censusObj;
     }
