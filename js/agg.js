45d34932b72742cf8f1bf678499c6a679e265640
Chris <chrisliow@gmail.com>
2016-09-11 01:25:42 -0500

    commit before testing apache

--- js/agg.js
+++ js/agg.js
@@ -15,44 +15,54 @@ function displayAggregates(obj, aggObj, displayHtml = '') {
   var varPercentHousingCrowded = computePercentVariance(obj.percentHousingCrowded, aggObj.avgPercentHousingCrowded);
   returnHtml += displayVariances(varPercentHousingCrowded, 
       "percentHousingCrowded",
-      "Crowded housing percentage is " + obj.percentHousingCrowded + "% (" + varPercentHousingCrowded + "% from the average).");
+      "Crowded housing percentage is " + obj.percentHousingCrowded + "% (" + varPercentHousingCrowded);
   returnHtml += '<hr>';
   var varPercentHouseholdsBelowPoverty = computePercentVariance(obj.percentHouseholdsBelowPoverty, aggObj.avgPercentHouseholdsBelowPoverty);
   returnHtml += displayVariances(varPercentHouseholdsBelowPoverty, 
       "percentHouseholdsBelowPoverty",
-      "Percentage of households below poverty is " + obj.percentHouseholdsBelowPoverty + "% (" + varPercentHouseholdsBelowPoverty + "% from the average).");
+      "Percentage of households below poverty is " + obj.percentHouseholdsBelowPoverty + "% (" + varPercentHouseholdsBelowPoverty);
   returnHtml += '<hr>';
   var varPercentAged16Unemployed = computePercentVariance(obj.percentAged16Unemployed, aggObj.avgPercentAged16Unemployed);
   returnHtml += displayVariances(varPercentAged16Unemployed, 
       "percentAged16Unemployed",
-      "Percentage of unemployed people above 16 is " + obj.percentAged16Unemployed + "% (" + varPercentAged16Unemployed + "% from the average.)");
+      "Percentage of unemployed people above 16 is " + obj.percentAged16Unemployed + "% (" + varPercentAged16Unemployed);
   returnHtml += '<hr>';
   var varPercentAged25NoDiploma = computePercentVariance(obj.percentAged25NoDiploma, aggObj.avgPercentAged25NoDiploma);
   returnHtml += displayVariances(varPercentAged25NoDiploma, 
       "percentAged25NoDiploma",
-      "Percentage of people above 25 with no high school diploma is " + obj.percentAged25NoDiploma + "% (" + varPercentAged25NoDiploma + "% from the average).");
+      "Percentage of people above 25 with no high school diploma is " + obj.percentAged25NoDiploma + "% (" + varPercentAged25NoDiploma);
   returnHtml += '<hr>';
   var varPercentAgedUnder18orOver64 = computePercentVariance(obj.percentAgedUnder18orOver64, aggObj.avgPercentAgedUnder18orOver64);
   returnHtml += displayVariances(varPercentAgedUnder18orOver64, 
       "percentAgedUnder18orOver64",
-      "Percentage of people under 18 and over 64 is " + obj.percentAgedUnder18orOver64 + "% (" + varPercentAgedUnder18orOver64 + "% from the average).");
+      "Percentage of people under 18 and over 64 is " + obj.percentAgedUnder18orOver64 + "% (" + varPercentAgedUnder18orOver64);
   returnHtml += '<hr>';
   var varPerCapitaIncome = computePercentVariance(obj.perCapitaIncome, aggObj.avgPerCapitaIncome);
   returnHtml += displayVariances(varPerCapitaIncome, 
       "perCapitaIncome",
-      "Per Capita income is $" + obj.perCapitaIncome + " (" + varPerCapitaIncome + "% from the average).");
+      "Per Capita income is $" + obj.perCapitaIncome + " (" + varPerCapitaIncome);
 
   return returnHtml;
 }
 
+function tallyNScore(obj, aggObj) {
+	
+    //var percentage = (((objVal - aggVal ) / aggVal) * 100).toFixed(2);
+    
+}
+
 function computePercentVariance(objVal, aggVal) {
-	return (((objVal - aggVal ) / aggVal) * 100).toFixed(2);
+	var percentage = (((objVal - aggVal ) / aggVal) * 100).toFixed(2);
+    if(percentage >= 0) { return percentage + '% above average)'; }
+    return percentage + '% below average)'
 }
 
 function displayVariances(variance, metricName, message) {
-	if(variance < -7) {
+    console.log(variance);
+    var numVar = parseFloat(variance.split('%')[0]);
+	if(numVar < -7) {
 		return '<div id="'+ metricName + '" class="metricLessThan">' + message + '</div>';
-	} else if( variance > 7){
+	} else if(numVar > 7){
 		return '<div id="'+ metricName + '" class="metricGreaterThan">' + message + '</div>';
 	}
 
--- map.php
+++ map.php
@@ -31,7 +31,7 @@
       <div id="mapContainer"></div>
       <div id="mapSidebar" class="col col-md-4">
         <div id="mapButtons">
-          <h2>Generic Header</h2>
+          <h2>Chicago HotSpots</h2>
             <div class="btn-group btn-group-justified" role="group" style="padding-bottom: 20px;">
               <div class="btn-group" role="group">
                 <button type="button" class="btn btn-default active" id="housing-button">Info</button>
