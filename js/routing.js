ef0f18d56ec3f83eda3be796e0a8d19e8b066a62
Chris <chrisliow@gmail.com>
2016-09-10 21:44:42 -0500

    ..

--- js/routing.js
+++ js/routing.js
@@ -9,6 +9,13 @@ var getRouting = function() {
 
 function markLocation(location) {
   console.log(location);
+  var currentPoint = new Point(, );
+  var size = new H.math.Size(40,40);
+  var markerIcon = new H.map.Icon('img/marker.png',{size:size});
+  var coords = { lat: location.coords.latitude, lng: location.coords.longitude };
+  marker = new H.map.Marker(coords, {icon: markerIcon, data:obj});
+  // return marker object
+  return marker;
 }
 
 function locationError(msg) {
